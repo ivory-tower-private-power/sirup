1811cd1f76a3260ffcc1ed81493f6a4076d51155
fhafner <hafner.flavio@gmail.com>
2023-06-06 11:28:48 +0200

    fix requests issue with ifconfig

--- src/IPRotator.py
+++ src/IPRotator.py
@@ -5,15 +5,34 @@ from getpass import getpass
 from random import Random 
 import logging 
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry 
 
 # TODO: copy over documentation/comment from the rotate_surfshark file.
 
+
 def get_ip(echo=False):
     "Query the current IP address of the computer."
-    result = requests.get("https://ifconfig.me")
-    if echo:
-        logging.info(f"IP is: {result.text}")
-    return result.text 
+    # sources:
+    # https://stackoverflow.com/questions/23013220/max-retries-exceeded-with-url-in-requests,
+    # https://urllib3.readthedocs.io/en/stable/reference/urllib3.util.html
+    session = requests.Session()
+    retry = Retry(total=3, backoff_factor=0.5)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount("https://", adapter)
+    session.mount("http://", adapter)
+
+    try:
+        response = session.get("https://ifconfig.me", timeout=3)
+        if response.ok:
+            if echo:
+                logging.info(f"IP is: {response.text}")
+            return response.text
+        else:
+            requests.ConnectionError("Failed to get the IP address")
+    except Exception as e:
+        logging.info(f"Got an exception: {e}")
+        requests.ConnectionError("Failed to get the IP address")
 
 
 class IPRotator():
