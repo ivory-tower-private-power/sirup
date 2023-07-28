e1133e4730bb19b4b2034795e63d0bd0cd5f78c8
Flavio Hafner <f.hafner@esciencenter.nl>
2023-07-28 16:01:41 +0200

    Tidy VPNConnector
    - move util functions away
    - use Popen context manager for handling openvpn errors

--- sirup/VPNConnector.py
+++ sirup/VPNConnector.py
@@ -3,48 +3,12 @@
 # import logging # TODO: add logging properly
 import os
 import subprocess
-import time
 from subprocess import PIPE
-from .get_ip import get_ip
+from .raise_ovpn_exceptions import raise_ovpn_exceptions
 from .TemporaryFileWithRootPermission import TemporaryFileWithRootPermission
-
-
-def sudo_read_file(file, pwd=None): # TODO: move this to utils?
-    "Read a file with root permission" 
-    # TODO: the if check here is not covered in tests
-    if isinstance(file, TemporaryFileWithRootPermission):
-        file = file.file_name
-    cmd = ["cat", file]
-    if pwd is None:
-        output = subprocess.run(cmd, capture_output=True, check=True)
-    else:
-        cmd.insert(0, "sudo")
-        output = subprocess.run(cmd, input=pwd.encode(), capture_output=True, check=True)
-    
-    content = output.stdout.decode()
-    content = content.rstrip().split("\n")
-    return content 
-
-
-def check_connection(log_file, timeout, pwd):
-    "Wait and test for established connection until `timeout`."
-    start_time = time.time()
-    connected = False
-    while time.time() - start_time < timeout and not connected:
-        time.sleep(2)
-        log = sudo_read_file(file=log_file, pwd=pwd)
-        connected = "Initialization Sequence Completed" in log[-1]
-
-    return connected 
-    
-def check_password(pwd): # TODO: use this only in the rotator class and test it when writing this.
-    "Run simple command to see if password is correct"
-    with subprocess.Popen(['sudo', "-S", "ls"], stdin=PIPE, stdout=PIPE, stderr=PIPE) as proc:
-        _, stderr = proc.communicate(input=f"{pwd}\n".encode())
-        if proc.returncode != 0:
-            raise RuntimeError("Wrong password")
-        # print(stdout)
-        print(stderr.decode())
+from .utils import check_connection
+from .utils import get_ip
+from .utils import sudo_read_file
 
 
 class VPNConnector():
@@ -58,10 +22,8 @@ class VPNConnector():
         self.base_ip = ip
         self._vpn_process_id = None # if not connected, this should be None
 
-        # TODO: assert that file exists etc..? or just raise an exception?
-
     def start_vpn(self, pwd, proc_id=None):
-        "Start an open vpn connection."
+        "Start an OpenVPN connection."
         self.log_file = TemporaryFileWithRootPermission(password=pwd, suffix=".log")
         self.log_file.create(file_name="openvpn")
         cmd = ["sudo", "-S", "openvpn",
@@ -72,11 +34,15 @@ class VPNConnector():
         
         if proc_id is not None:
             cmd.extend(["--writepid", proc_id])
-        subprocess.run(cmd, input=pwd.encode(), check=True)
-        # Wrong inputs to openvpn (auth or config file) are printed to console;
-        # the log file is only created when openvpn "finds" the destination server
-        # If this is not the case, capture it here. It's hacky, but I don't know
-        # a better way right now.
+        with subprocess.Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE) as proc:
+            stdout, stderr = proc.communicate(pwd.encode()) 
+            if proc.returncode != 0: 
+                log = None 
+                if os.path.exists(self.log_file.file_name):
+                    log = sudo_read_file(file=self.log_file.file_name, pwd=pwd) # what happens if the log file does not exist?
+                raise_ovpn_exceptions(stdout.decode(), stderr.decode(), log)
+                # process the log file, stdout and stderr here 
+
         if not os.path.exists(self.log_file.file_name): # FIXME: this makes the test_start_vpn* fail 
             raise FileNotFoundError("Openvpn connection failed. Are the supplied file names correct?")
 
@@ -94,6 +60,7 @@ class VPNConnector():
             else:
                 raise TimeoutError("Could not connect to vpn") 
             # TODO: should this be a specific openvpn connection error? not sure
+            # TODO: print the log file? last messages of the log file?
 
 
     def disconnect(self):
