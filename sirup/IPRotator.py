4ffbb3ca895fd6a53b7711100c6f25d0669dfe05
Flavio Hafner <f.hafner@esciencecenter.nl>
2023-08-04 14:45:19 +0200

    integrate connector into IPRotator

--- sirup/IPRotator.py
+++ sirup/IPRotator.py
@@ -1,155 +1,58 @@
 import getpass
 import logging
-import os
-import subprocess
 import time
 from random import Random
-from .get_ip import get_ip
-from .TemporaryFileWithRootPermission import TemporaryFileWithRootPermission
+from .utils import RotationList
+from .utils import list_files_with_full_path
+from .VPNConnector import VPNConnector
 
-
-def start_vpn(config, auth, log, proc_id, pwd):
-    "Start an open vpn connection."
-    cmd = ["sudo", "-S", "openvpn",
-           "--config", config,
-           "--auth-user-pass", auth,
-           "--log", log,
-           "--writepid", proc_id,
-           "--daemon"]
-
-    logging.debug("start openvpn")
-    subprocess.run(cmd, input=pwd.encode(), check=True)
 
 class IPRotator():
-    def __init__(self, auth_file, log_file, config_location, seed=123, pwd=None): # pylint: disable=too-many-arguments
+    def __init__(self, auth_file, config_location, pwd=None, seed=123, config_file_rule=None): # pylint: disable=too-many-arguments
         # TODO: docstring for parameters
         # TODO: require types? config_location should be os.path? then remove the pylint constraint above
+        # TODO: how to deal with properties from the VPNconnector? ie IP, is connected, base IP, ...
+        
+        config_files = list_files_with_full_path(config_location, config_file_rule)
+        self.config_queue = RotationList(config_files)
         self.auth_file = auth_file
-        self.log_file = log_file
-        self.config_location = config_location
-        self.config_files = None 
-        self.is_connected = False
-        ip = get_ip()
-        self.current_ip = ip 
-        self.base_ip = ip
-        self.current_config_file = None
-        self.vpn_process_id = None 
         self.randomizer = Random(seed)
-
         if pwd is None:
             pwd = getpass.getpass("Please enter your sudo password: ")
-        self.pwd = pwd
+        self.pwd = pwd       # TODO: validate password? ie try `sudo ls`, and if it fails, raise an exception?
+        self.connector = None # TODO: better name?
 
-        self._load_config_files() # TODO: this makes the tests fail. how to fix?
-        # TODO: validate password? ie try `sudo ls`, and if it fails, raise an exception?
-
-    def _load_config_files(self):
-        "Take all available confg file and put them into a list. Only use _udp connections; tcp and upd give the same IP for a given server."
-        # TODO: remove this? ie ask directly for the list of config files?
-        files = [f for f in os.listdir(self.config_location) if "-tor" not in f] #ignore tor proxies. could be slow to establish connection.
-        files = [os.path.join(self.config_location, f) for f in files]
-        self.config_files = files
-
-    def _connect(self, timeout=40):
-        "Connect to single server. Fails when no connection after `timeout`."
-        if self.current_config_file is None: # set IP for the first time 
-            self._set_config_file()
-
-        logging.debug("start _connect")
-        # TODO: capture missing config file? missing auth file? how do I do this?
-
-        with TemporaryFileWithRootPermission(suffix=".txt", password=self.pwd) as file_with_process_id:
-            start_vpn(self.current_config_file, self.auth_file, self.log_file, file_with_process_id, self.pwd)
-
-            start_time = time.time()
-            while time.time() - start_time <= timeout and not self.is_connected:
-                time.sleep(3)
-                self.is_connected = self._check_connection()
-            
-            if self.is_connected:
-                logging.info("Connected with %s", self.current_config_file)
-                self.current_ip = get_ip()
-            else:
-                raise TimeoutError(f"Could not build connection with {self.current_config_file}")
-
-            logging.debug("reading in vpn pid")
-            with open(file_with_process_id, "rb") as file: # TODO: this can be separated into a function and tested separately
-                vpn_pid = file.readlines()
-                assert len(vpn_pid) == 1, "Unexpected length of file."
-                self.vpn_process_id = vpn_pid[0].strip()
-
-
-
-    def rotate(self):
-        "Rotate to next server"
-        self.disconnect()
-        self._set_config_file()
-        self.connect()
-
-    def connect(self):
-        "Connect to the next server in the queue"
-        i = 0
-        max_trials = 2000 # set to some high value 
+    def connect(self, shuffle=False, max_trials=2000):
+        "Connect to server associated with the first config_file in the list."
+        i = 0 
+        if shuffle:
+            self.config_queue.shuffle(self.randomizer)
+        # try to connect; if it fails, change the server and retry
         while True:
-            try: 
-                self._connect()
+            connector = VPNConnector(self.config_queue.pop_append(), self.auth_file)
+            try:
+                connector.connect(pwd=self.pwd)
             except TimeoutError as e:
                 i += 1
-                if i > max_trials:
+                if i >= max_trials:
                     raise TimeoutError(f"Failed to connect to {max_trials} different servers.") from e 
-                logging.info("Handling error %s", e)
-                self._set_config_file()
                 waiting_time = 10
                 if i % 20 == 0:
                     waiting_time = 300 # try to see whether this helps
                     logging.info("Failed to connect %d times; waiting %d", i, waiting_time)
-                time.sleep(waiting_time) 
-            if self.is_connected:
+                time.sleep(waiting_time)
+            
+            if connector.is_connected():
                 break
 
-    def _set_config_file(self):
-        "Set or update the config file to the next candidate in the list."
-        # TODO: use here, as a util, the function I wrote in the other repo? 
-        # (for pop_append?) need to return both the updated list (or not b/c mutable?) and set the config file
-        if self.current_config_file is not None:
-            self.config_files.append(self.current_config_file) # put the current file to the end of the queue 
-        self.current_config_file = os.path.join(self.config_files.pop(0))
+        self.connector = connector
 
-    def shuffle_proxies(self):
-        "Randomly shuffle the list of proxies. This changes the order by which we iterate through them."
-        self.randomizer.shuffle(self.config_files)
-        self._set_config_file()
+    
+    def disconnect(self):
+        self.connector.disconnect(self.pwd)
+        self.connector = None
 
-
-    def disconnect(self, check_ip=False):
-        "Disconnect the vpn, get back to base IP."
-        cmd = ["sudo", "-S", "kill", self.vpn_process_id]
-        subprocess.run(cmd, input=self.pwd.encode(), check=True)
-        time.sleep(5)
-        self.current_ip = get_ip(echo=True)
-        if check_ip: # this is to make sure the IP is the same as when the class was instantiated. 
-            # it may not be necessary, and could even a problem in networks with dynamic IPs (like eduroam)
-            assert self.current_ip == self.base_ip
-        self.is_connected = False
-
-
-    def _check_connection(self, print_connection=False):
-        # TODO: test correctly reading log file; test reaction to different log file messages/print the whole logfile?
-        log = self._read_logfile()
-        if print_connection:
-            for line in log:
-                if "Peer Connection Initiated" in line:
-                    logging.info(line)
-        if "Initialization Sequence Completed" in log[-1]:
-            return True 
-        return False 
-
-
-    def _read_logfile(self):
-        "read log file from openvpn into a list"
-        # TODO: check that log file is read correctly
-        cmd = ["sudo", "cat", self.log_file]
-        output = subprocess.run(cmd, input=self.pwd.encode(), capture_output=True, check=True)
-        content = output.stdout.decode()
-        content = content.rstrip().split("\n")
-        return content 
+    def rotate(self):
+        "Rotate to next server"
+        self.disconnect()
+        self.connect()
--- tests/test_IPRotator.py
+++ tests/test_IPRotator.py
@@ -1,31 +1,22 @@
 """Tests for the sirup.IPRotator module.
 """
 
-import subprocess
+from unittest import mock
 from sirup.IPRotator import IPRotator
-from sirup.IPRotator import start_vpn
+from sirup.utils import RotationList
 
 
-def test_IPRotator():
-    pass
+@mock.patch("getpass.getpass")
+def test_instantiate(mock_getpass, tmp_path):   
+    config_files = ["file1", "file2", "file3"] # TODO: make this fixture available across tests. similar instance used in test_utils.py
+    for f in config_files:
+        (tmp_path / f).touch()
+    
+    mock_getpass.return_value = "my_password"
+    instance = IPRotator("path/to/auth/file", tmp_path)
 
-
-def test_class_instantiation_with_mocked_password(mocker):
-    mocker.patch("getpass.getpass", return_value="test_password")
-    instance = IPRotator(auth_file="file1", log_file="file2", config_location="file3")
-    assert instance.pwd == "test_password"
-
-def test_start_vpn(mocker):
-    mocker.patch("subprocess.run")
-    start_vpn("myconfig", "myauth", "mylog", "myproc_id", "mypwd")
-    cmd = ["sudo", "-S", "openvpn",
-           "--config", "myconfig",
-           "--auth-user-pass", "myauth",
-           "--log", "mylog",
-           "--writepid", "myproc_id",
-           "--daemon"]
-    subprocess.run.assert_called_once_with(cmd, input="mypwd".encode(), check=True) 
-
-# def test_class_instantiation_with_provided_password():
-#     instance = YourClass(arg1="test", password="test_password")
-#     assert instance.password == "test_password"
+    assert instance.connector is None, "connector wrongly instantiated"
+    assert instance.pwd == "my_password"
+    mock_getpass.assert_called_once()
+    assert instance.auth_file == "path/to/auth/file"
+    assert isinstance(instance.config_queue, RotationList)
