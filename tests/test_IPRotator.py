e640fa8e6fd8937db523a7b5aeb7cc7ccbd258c6
Flavio Hafner <f.hafner@esciencecenter.nl>
2023-10-30 15:43:57 +0100

    test repr of IP rotator

--- tests/test_IPRotator.py
+++ tests/test_IPRotator.py
@@ -25,3 +25,24 @@ def test_instantiate(mock_getpass, mock_kill, mock_check_pw, tmp_path):
     assert instance.auth_file == "path/to/auth/file"
     assert isinstance(instance.config_queue, RotationList)
     mock_kill.assert_called_once_with("my_password")
+
+
+@mock.patch("sirup.IPRotator.check_password")
+@mock.patch("sirup.IPRotator.kill_all_connections")
+@mock.patch("getpass.getpass")
+def test_repr(mock_getpass, mock_kill, mock_check_pw, tmp_path):# pylint: disable=unused-argument
+    # NOTE: tmp_path is a built-in fixture of mock
+    config_files = ["file1", "file2", "file3"] # TODO: make this fixture available across tests. similar instance used in test_utils.py
+    for f in config_files:
+        (tmp_path / f).touch()
+    
+    mock_getpass.return_value = "my_password"
+    mock_check_pw.return_value = True
+    instance = IPRotator("path/to/auth/file", tmp_path)
+    repr_output = repr(instance)
+    #pylint: disable=protected-access
+    repr_expected = "IPRotator(auth_file='path/to/auth/file', "\
+         f"pwd=<SECRET>, track_ip=True, config_location={instance._other_inputs['config_location']}, "\
+         f"seed={instance._other_inputs['seed']}, config_file_rule={instance._other_inputs['config_file_rule']})"
+    assert repr_output == repr_expected
+    #pylint: enable=protected-access
