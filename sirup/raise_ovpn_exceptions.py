218cf54c54ae891da0c8464e44364a6f2d7b9f90
Flavio Hafner <f.hafner@esciencecenter.nl>
2023-11-30 16:09:18 +0100

    refactor error raising; closes #10

--- sirup/raise_ovpn_exceptions.py
+++ sirup/raise_ovpn_exceptions.py
@@ -11,25 +11,15 @@ def raise_ovpn_exceptions(stdout, stderr, log):
             - when there is a problem with the configuration file
             - in any other case
     """
-    base_error_message = None
     if log is not None:
         msg0 = "Options error: --auth-user-pass fails with"
         msg1 = "No such file or directory"
         if lookup_strings_in_list([msg0, msg1], log):
             raise FileNotFoundError("Wrong authentication file.")
-        # print other information here for unanticipated situations
-        base_error_message = f"""
-        Log content:\n {" ".join(log)},
-        stdout content:\n {stdout}
-        """
-    elif stdout != "":
-        if "Error opening configuration file:" in stdout:
-            raise RuntimeError("Problem with configuration file:")
-        base_error_message = f"stdout content: {stdout}"
-    else: # unanticipated stderr cases
-        base_error_message = f"""
-            stderr content:\n {stderr}
-            """
-    
-    if base_error_message is not None:
-        raise RuntimeError(base_error_message)
+        # print other information here for unanticipated situations        
+        raise RuntimeError(f"Log content:\n {' '.join(log)}, stdout content:\n {stdout}")
+    if "Error opening configuration file:" in stdout:
+        raise RuntimeError("Problem with configuration file:")    
+    if stdout == "": # unanticipated stderr cases
+        raise RuntimeError(f"stderr content:\n {stderr}")
+    raise RuntimeError(f"stdout content: {stdout}")
